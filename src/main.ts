43863db31bdee6a61586a997a430f13d4ac31c61
HikaruShiho <shiho97797@gmail.com>
2024-03-02 15:31:10 +0900

    アプリケーション層エラーハンドリング

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import morgan from 'morgan'
+import { ApplicationError } from './application/error/applicationError'
 import { DomainError } from './domain/error/DomainError'
 import { gameRouter } from './presentation/gameRouter'
 import { turnRouter } from './presentation/turnRouter'
@@ -39,6 +40,17 @@ function errorHandler(
       message: err.message
     })
     return
+  }
+
+  if (err instanceof ApplicationError) {
+    switch (err.type) {
+      case 'LatestGameNotFound':
+        res.status(404).json({
+          type: err.type,
+          message: err.message
+        })
+        return
+    }
   }
 
   console.error('Unexpected error occurred', err)
